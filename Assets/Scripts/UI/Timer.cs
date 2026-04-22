6e5ddd6b59f5de8ed150c25929928dac72115a81
Denoraa <108433209+Denoraa@users.noreply.github.com>
2026-04-22 16:21:12 -0700

    1

---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private float time = 0f;
+    private TextMeshProUGUI timerText;
+    private void OnEnable()
+    {
+        EventBus.Subscribe
+    }
+    
+    private void OnDisable()
+    {
+        EventBus.Unsubscribe<float>(EventType.OnGameStart, OnGameStart);
+    }
+
+}
