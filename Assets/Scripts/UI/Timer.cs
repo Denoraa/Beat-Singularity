4992c337fdab60a20c88a2ba74266dbe1b4dc00c
Denoraa <3377545725@qq.com>
2026-04-22 17:43:39 -0700

    1

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    private float time = 0f;
-    private TextMeshProUGUI timerText;
-    private void OnEnable()
-    {
-        EventBus.Subscribe
-    }
+    // private float time = 0f;
+    // private TextMeshProUGUI timerText;
+    // private void OnEnable()
+    // {
+    //     EventBus.Subscribe<
+    // }
     
-    private void OnDisable()
-    {
-        EventBus.Unsubscribe<float>(EventType.OnGameStart, OnGameStart);
-    }
+    // private void OnDisable()
+    // {
+    //     EventBus.Unsubscribe<float>(EventType.OnGameStart, OnGameStart);
+    // }
 
 }
