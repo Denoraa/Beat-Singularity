2f29a09ed85ad63872dd8a4630914af2bba6f741
Denoraa <108433209+Denoraa@users.noreply.github.com>
2026-04-27 13:39:28 -0700

    Update ActiveNoteManager.cs

--- Assets/Scripts/HitSystem/ActiveNoteManager.cs
+++ Assets/Scripts/HitSystem/ActiveNoteManager.cs
@@ -1,23 +1,25 @@
 using System.Collections.Generic;
-using UnityEngine;
 
-public class ActiveNoteManager :Singleton<ActiveNoteManager>
+public class ActiveNoteManager : Singleton<ActiveNoteManager>
 {
-
     private List<Note> activeNotes = new List<Note>();
 
     public void RegisterNote(Note note)
     {
+        if (note == null) return;
+
         if (!activeNotes.Contains(note))
             activeNotes.Add(note);
     }
 
     public void UnregisterNote(Note note)
     {
+        if (note == null) return;
+
         activeNotes.Remove(note);
     }
 
-    public List<Note> GetActiveNotes()
+    public IReadOnlyList<Note> GetActiveNotes()
     {
         return activeNotes;
     }
